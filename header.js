c08ad0f6edc761e7339460535a29b911504ae10b
carlesonc <christopher.carleson@gmail.com>
2020-04-08 17:37:48 +0200

    Adding search to nav-bar after scrolling
    Det buggar ibland när man skrollar upp och ner, sökbaren och logon stannar i nav istället för att hoppa upp till header-top.

--- header.js
+++ header.js
@@ -1,10 +1,20 @@
 // När användaren skrollar ner 100px så göms övre delen av header och sökfältet dyker upp i menyraden
 window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {setTimeout(() => { scrollFunction(); },300)};
+
+const headerTop = document.getElementById("header-top");
+const headLogo = document.getElementById("header-logo-main");
+const headSearch = document.getElementById("header-search");
+const headNav = document.getElementById("header-nav");
 
 function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    document.getElementById("header-top").style.maxHeight = "0px";
-} else {
-    document.getElementById("header-top").style.maxHeight = "1000px";
-  }
-} 
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.getElementById("header-top").style.maxHeight = "0px";
+        setTimeout(() => { headNav.appendChild(headSearch); }, 300);
+        setTimeout(() => { headNav.prepend(headLogo); }, 300);
+    } else {
+        document.getElementById("header-top").style.maxHeight = "1000px";
+        headerTop.prepend(headSearch);
+        headerTop.prepend(headLogo);
+    }
+}

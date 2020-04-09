fe89e13651daa26981f66103490e90413cff9fba
rstina <53047495+rstina@users.noreply.github.com>
2020-04-09 11:47:03 +0200

    updated css for prods, db

--- read.php
+++ read.php
@@ -1,3 +1,11 @@
+<div class="products__display">
+  <nav class="">
+      <a href="index.php?id=alla">Alla produkter</a>
+      <a href="index.php?id=mat">Mat</a>
+      <a href="index.php?id=hygien">Hygien</a>
+      <a href="index.php?id=blommor">Blommor</a>
+    </nav>
+
 <?php
 /**************************************** *
  * read info from db & display posts
@@ -11,23 +19,23 @@
   if(isset($_GET['id'])){
     $id = htmlentities($_GET['id']);
     if($id == 'mat'){
-      $sql = "SELECT * FROM products 
-              WHERE category = 'mat'
+      $sql = "SELECT * FROM product 
+              WHERE category_id = '2'
               ORDER BY name";
     } elseif($id == 'hygien'){
-      $sql = "SELECT * FROM products 
-              WHERE category = 'hygien'
+      $sql = "SELECT * FROM product 
+              WHERE category_id = '1'
               ORDER BY name";
     } elseif($id == 'alla'){
-      $sql = "SELECT * FROM products 
+      $sql = "SELECT * FROM product 
               ORDER BY name";
     } elseif($id == 'blommor'){
-      $sql = "SELECT * FROM products 
-              WHERE category = 'blommor'
+      $sql = "SELECT * FROM product 
+              WHERE category_id = '3'
               ORDER BY name";
     }
   } else {
-    $sql = "SELECT * FROM products 
+    $sql = "SELECT * FROM product 
             ORDER BY name";
   }
 
@@ -35,32 +43,41 @@
   $stmt->execute();
 
   // starta div för inlägg
-  echo "<div class='container'>";
+  echo "<div class='products__container'>";
   // loopar över arrayen som har resultatet från db
   while($row = $stmt->fetch(PDO::FETCH_ASSOC)) :
       // spara data från db i varsin variabel
-      $id = htmlspecialchars($row['productID']); // $row = array
+      $id = htmlspecialchars($row['product_id']); // $row = array
       $heading = htmlspecialchars($row['name']);
-      $description = htmlspecialchars($row['description']);
-      $category = strtoupper(htmlspecialchars($row['category']));
-      $storage = htmlspecialchars($row['storage']);
-      if($storage > 0){
-        $storageText = "Antal i lager - ".$storage;
+      // $description = htmlspecialchars($row['description']);
+      $category = strtoupper(htmlspecialchars($row['category_id']));
+      $quantity = htmlspecialchars($row['quantity']);
+      $price = htmlspecialchars($row['price']);
+      if($quantity > 0){
+        $quantityText = "Antal i lager - ".$quantity;
       } else {
-        $storageText = "Ej i lager";
+        $quantityText = "Ej i lager";
       }
       echo "<br>
-        <div class='products__container'>
           <div class='products__item'>
-            <p class=''>$category</p>
-            <h2>$heading</h2>
-            <p>$description</p>
-            <p class=''>$storageText</p>
+            <div class='products__item--img'>
+            <br>
+            <p>Plats för bild</p>
+            <br>
+            </div>
+            <div class='products__item--text'>
+              <h2>$heading</h2>
+              <p>$price</p>
+              <a href='#' class='product__btn'>Köp</a>
+              <p class=''>$quantityText</p>
+            </div>
           </div>
-        </div>
         <br>
         "; 
   // avsluta while loop
   endwhile;
 // stäng post div
   echo "</div>";
+?>
+</div>
+

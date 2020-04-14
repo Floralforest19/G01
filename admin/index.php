cc9af306618acaabba81226e8be3b1fd2fbd7753
rstina <53047495+rstina@users.noreply.github.com>
2020-04-14 09:11:17 +0200

    Read sort categories (#5)
    * prods-categories sort, connect to db, admin map
    
    * minor error fix
    
    * updated css for prods, db
    
    * some css

--- admin/index.php
+++ admin/index.php
@@ -4,12 +4,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="styles/style.css">
-  <title>Webbshop</title>
+  <title>Kris & Ros - Admin</title>
 </head>
 <body>
-  <h1>Hello, World!</h1>
-  <?php require_once 'read.php'; ?>
-  <p>Kopplas ihop med rätt databas, se db.php, read.php</p>
+  <h1>Admin</h1>
 </body>
 </html>
---
+++ read.php
@@ -0,0 +1,85 @@
+<?php
+/**************************************** *
+ * read info from db & display posts
+ * different sql-orders depending on which link pushed
+ * filters according to categories
+**************************************** */
+
+  // koppla till databas
+  require_once 'db.php';
+?>
+
+<div class='products__display'>
+  <nav class='products__menu'>
+    <a href='index.php?id=all'>Alla produkter</a>
+    <a href='index.php?id=food'>Mat</a>
+    <a href='index.php?id=hygien'>Hygien</a>
+    <a href='index.php?id=roses'>Rosor</a>
+   </nav>
+
+<?php
+  if(isset($_GET['id'])){
+    $id = htmlentities($_GET['id']);
+    if($id == 'food'){
+      $sql = "SELECT * FROM product 
+              WHERE category_id = '2'
+              ORDER BY name";
+    } elseif($id == 'hygien'){
+      $sql = "SELECT * FROM product 
+              WHERE category_id = '1'
+              ORDER BY name";
+    } elseif($id == 'all'){
+      $sql = "SELECT * FROM product 
+              ORDER BY name";
+    } elseif($id == 'roses'){
+      $sql = "SELECT * FROM product 
+              WHERE category_id = '3'
+              ORDER BY name";
+    }
+  } else {
+    $sql = "SELECT * FROM product 
+            ORDER BY name";
+  }
+
+  $stmt = $db->prepare($sql);
+  $stmt->execute();
+
+  // starta div för inlägg
+  echo "<div class='products__container'>";
+  // loopar över arrayen som har resultatet från db
+  while($row = $stmt->fetch(PDO::FETCH_ASSOC)) :
+      // spara data från db i varsin variabel
+      $id = htmlspecialchars($row['product_id']); // $row = array
+      $heading = htmlspecialchars($row['name']);
+      // $description = htmlspecialchars($row['description']);
+      $category = strtoupper(htmlspecialchars($row['category_id']));
+      $quantity = htmlspecialchars($row['quantity']);
+      $price = htmlspecialchars($row['price']);
+      if($quantity > 0){
+        $quantityText = "Antal i lager - ".$quantity;
+      } else {
+        $quantityText = "Ej i lager";
+      }
+      echo "<br>
+          <div class='products__item'>
+            <div class='products__item--img'>
+            <br>
+            <p>Plats för bild</p>
+            <br>
+            </div>
+            <div class='products__item--text'>
+              <h2>$heading</h2>
+              <p>$price</p>
+              <a href='#' class='product__btn'>Köp</a>
+              <p class=''>$quantityText</p>
+            </div>
+          </div>
+        <br>
+        "; 
+  // avsluta while loop
+  endwhile;
+// stäng post div
+  echo "</div>";
+?>
+</div>
+

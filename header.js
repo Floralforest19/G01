70116a756d3ca37d1ce6db849588ea55fcbed15f
carlesonc <christopher.carleson@gmail.com>
2020-04-15 11:00:24 +0200

    Nu ser menyn bra ut på mobil.
    Behöver anpassas för desktop.

--- header.js
+++ header.js
@@ -2,19 +2,34 @@
 window.onscroll = function() {scrollFunction()};
 // window.onscroll = function() {setTimeout(() => { scrollFunction(); },300)};
 
+const header = document.getElementById("header");
 const headerTop = document.getElementById("header-top");
 const headLogo = document.getElementById("header-logo-main");
 const headSearch = document.getElementById("header-search");
 const headNav = document.getElementById("header-nav");
+const dropdownMenu = document.getElementById("dropdownMenu");
 
 function scrollFunction() {
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        document.getElementById("header-top").style.maxHeight = "0px";
-        setTimeout(() => { headNav.appendChild(headSearch); }, 300);
-        setTimeout(() => { headNav.prepend(headLogo); }, 300);
+        headerTop.style.maxHeight = "0px";
+
+        // setTimeout(() => { headNav.appendChild(headSearch); }, 300);
+        headLogo.style.maxHeight = headerTop.style.maxHeight;
     } else {
-        document.getElementById("header-top").style.maxHeight = "1000px";
-        headerTop.prepend(headSearch);
-        headerTop.prepend(headLogo);
+        headerTop.style.maxHeight = "unset";
+
+        // headerTop.prepend(headSearch);
+        // headerTop.prepend(headLogo);
+        headLogo.style.maxHeight = "100%";
     }
 }
+
+headNav.addEventListener("click", function () {
+    headNav.classList.toggle("change");
+
+    dropdownMenu.classList.toggle("show");
+    // if (dropdownMenu.style.display === "none") {
+    //     dropdownMenu.style.display = "unset";
+        
+    // }
+});

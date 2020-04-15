850defa881af7f78ed14121ac0a6b5e3163875af
rstina <53047495+rstina@users.noreply.github.com>
2020-04-15 13:52:27 +0200

    skapat css mall som fungerar som bas (#7)
    Co-authored-by: rebeckamolin <56819811+rebeckamolin@users.noreply.github.com>

--- index.php
+++ index.php
@@ -2,14 +2,119 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="styles/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" type="text/css" href="styles/style.css" />
+  <link
+      href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap"
+      rel="stylesheet"
+  />
   <title>Webbshop</title>
 </head>
 <body>
-  <h1>Hello, World!</h1>
-  <?php require_once 'read.php'; ?>
-  <p>Kopplas ihop med rätt databas, se db.php, read.php</p>
+
+ <!-- Bortkommenterat eftersom sidan inte fungerar om jag inte har kontakt med databas
+   
+ <h1>Hello, World!</h1>
+  <//?php require_once 'read.php'; ?>
+  <p>Kopplas ihop med rätt databas, se db.php, read.php</p> -->
+
+
+
+  <!-- Framework/mockup på utseende. Test för CSS -->
+<ul>
+        <li><a href="index.php">Hem</a></li>
+        <li><a href="singleproduct.html">En produkt</a></li>
+      </ul>
+    <main>
+
+      <section class="background">
+        <h2>Våra produkter</h2>
+
+        <div class="product__wrapper">
+          <article class="box">
+            <div class="box__pic">
+              <img src="./images/toalettpapper.jpg" alt="toalettpapper"/>
+            </div>
+            <div class="box__text">
+              <h3>Toalettpapper</h3>
+              <button>Lägg i varukorg</button>
+            </div>
+          </article>
+
+          <article class="box">
+            <div class="box__pic">
+              <img src="./images/toalettpapper.jpg" alt="toalettpapper"/>
+            </div>
+            <div class="box__text">
+              <h3>Toalettpapper</h3>
+              <button>Lägg i varukorg</button>
+            </div>
+          </article>
+
+          <article class="box">
+            <div class="box__pic">
+              <img src="./images/toalettpapper.jpg" alt="toalettpapper"/>
+            </div>
+            <div class="box__text">
+              <h3>Toalettpapper</h3>
+              <button>Lägg i varukorg</button>
+            </div>
+          </article>
+
+          <article class="box">
+            <div class="box__pic">
+              <img src="./images/toalettpapper.jpg" alt="toalettpapper"/>
+            </div>
+            <div class="box__text">
+              <h3>Toalettpapper</h3>
+              <button>Lägg i varukorg</button>
+            </div>
+          </article>
+          
+          <article class="box">
+            <div class="box__pic">
+              <img src="./images/toalettpapper.jpg" alt="toalettpapper"/>
+            </div>
+            <div class="box__text">
+              <h3>Toalettpapper</h3>
+              <button>Lägg i varukorg</button>
+            </div>
+          </article>
+          
+          <article class="box">
+            <div class="box__pic">
+              <img src="./images/toalettpapper.jpg" alt="toalettpapper"/>
+            </div>
+            <div class="box__text">
+              <h3>Toalettpapper</h3>
+              <button>Lägg i varukorg</button>
+            </div>
+          </article>
+
+          <article class="box">
+            <div class="box__pic">
+              <img src="./images/toalettpapper.jpg" alt="toalettpapper"/>
+            </div>
+            <div class="box__text">
+              <h3>Toalettpapper</h3>
+              <button>Lägg i varukorg</button>
+            </div>
+          </article>
+
+          <article class="box">
+            <div class="box__pic">
+              <img src="./images/toalettpapper.jpg" alt="toalettpapper"/>
+            </div>
+            <div class="box__text">
+              <h3>Toalettpapper</h3>
+              <button>Lägg i varukorg</button>
+            </div>
+          </article>
+
+      </section>
+    </main>
+  <!-- Slut på CSS-test -->
+
 </body>
 </html>

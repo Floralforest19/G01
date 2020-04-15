4c10dd0ed2113e6a9bc40fcb27502a5aa2e9f328
rstina <53047495+rstina@users.noreply.github.com>
2020-04-15 15:12:39 +0200

    read products from db

--- read.php
+++ read.php
@@ -10,13 +10,11 @@
 ?>
 
 <div class='products__display'>
-
   <nav class='products__menu'>
     <a href='index.php?id=all'>Alla produkter</a>
     <a href='index.php?id=food'>Mat</a>
     <a href='index.php?id=hygien'>Hygien</a>
     <a href='index.php?id=roses'>Rosor</a>
-
    </nav>
 
 <?php
@@ -46,14 +44,30 @@
   $stmt = $db->prepare($sql);
   $stmt->execute();
 
+
+
+  // <div class="product__wrapper">
+  //   <article class="box">
+  //     <div class="box__pic">
+  //       <img src="./images/toalettpapper.jpg" alt="toalettpapper"/>
+  //     </div>
+  //     <div class="box__text">
+  //       <h3>Toalettpapper</h3>
+  //       <button>Lägg i varukorg</button>
+  //     </div>
+  //   </article>
+
+
   // starta div för inlägg
-  echo "<div class='products__container'>";
+  echo "<section class='background'>
+        <h2>Våra produkter</h2>
+        <div class='product__wrapper'>";
   // loopar över arrayen som har resultatet från db
   while($row = $stmt->fetch(PDO::FETCH_ASSOC)) :
       // spara data från db i varsin variabel
       $id = htmlspecialchars($row['product_id']); // $row = array
-      $heading = htmlspecialchars($row['name']);
-      // $description = htmlspecialchars($row['description']);
+      $name = htmlspecialchars($row['name']);
+      $description = htmlspecialchars($row['description']);
       $category = strtoupper(htmlspecialchars($row['category_id']));
       $quantity = htmlspecialchars($row['quantity']);
       $price = htmlspecialchars($row['price']);
@@ -62,27 +76,29 @@
       } else {
         $quantityText = "Ej i lager";
       }
-      echo "<br>
-          <div class='products__item'>
-            <div class='products__item--img'>
-            <br>
-            <p>Plats för bild</p>
-            <br>
+
+      echo "
+          <article class='box'>
+            <div class='box__pic'>
+              <img src='./images/toalettpapper.jpg' alt='toalettpapper'/>
             </div>
-            <div class='products__item--text'>
-              <h2>$heading</h2>
-              <p>$price</p>
-              <a href='#' class='product__btn'>Köp</a>
-              <p class=''>$quantityText</p>
+            <div class='box__text'>
+              <h3>$name</h3>
+              <p>$price kr</p>
+              <p>$description</p>
+              <a href='showproduct.php?id=$id'>Läs mer</a><br>
+              <button>Lägg i varukorg</button>";
+              // läs mer bör gå till produktsidan
+              // lägga till när vi introducerar varukorg
+              //<a href='#' class='product__btn'>Köp</a>
+              echo "<p class=''>$quantityText</p>
             </div>
-          </div>
-        <br>
-        "; 
+          </article>"; 
+
   // avsluta while loop
   endwhile;
 // stäng post div
-  echo "</div>";
+  echo "</div></section>";
 ?>
-
 </div>
 

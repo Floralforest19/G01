70cb1734371b002bb9c01c93afe8e60769f0499a
Floralforest19 <56820062+Floralforest19@users.noreply.github.com>
2020-04-15 15:34:55 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,47 +2,26 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="styles/style.css">
-  <title>Kris&Ros</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" type="text/css" href="styles/style.css" />
+  <link
+      href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap"
+      rel="stylesheet"
+  />
+  <title>Webbshop</title>
 </head>
 <body>
-  <h1>Kris & Ros</h1>
-      <header id="header" class="header">
-        <div id="header-top" class="header-top">
 
-            <img class="header-logo-main" src="images\Skärmklipp.PNG" alt="temp logo">
+<ul>
+  <li><a href="index.php">Hem</a></li>
+  <li><a href="singleproduct.html">En produkt</a></li>
+  <li><a href="search.php">Sök</a></li>
+</ul>
+    <main>
+    <?php require_once 'read.php'; ?>
+    </main>
+  <!-- Slut på CSS-test -->
 
-            <input class="header-search" type="search" name="header-search" id="header-search" placeholder="Search...">
-
-            <div class="header-button">
-                <a class="header-button-a" href="">
-                    <img class="header-logo-button" src="./images/phone.png" alt="Phone">
-                    <p>Contact</p>
-                </a>
-                <a class="header-button-a" href="">
-                    <img class="header-logo-button" src="./images/shoppingcart.png" alt="Shopping cart">
-                    <p>Cart</p>
-                </a>
-            </div>
-
-        </div>
-
-        <nav class="header-nav">
-            <a class="header-nav-active" href="">Home</a>
-            <a href="/Applications/MAMP/htdocs/G01/products.html">Products</a>
-            <a href="">About</a>
-            <a href="">Contact</a>
-            <a href="">Basket</a>
-        </nav>
-    </header>
-    <script src="header.js"></script>
-
-
-  <?php 
-  require_once 'read.php'
-  ; 
-  ?>
 </body>
 </html>

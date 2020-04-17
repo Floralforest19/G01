b0359d450ba7906979016b9c3bd829bb23d035af
carlesonc <christopher.carleson@gmail.com>
2020-04-17 11:21:10 +0200

    Mobil-version klar.

--- header.js
+++ header.js
@@ -1,28 +1,33 @@
-// När användaren skrollar ner 100px så göms övre delen av header och sökfältet dyker upp i menyraden
-window.onscroll = function() {scrollFunction()};
-// window.onscroll = function() {setTimeout(() => { scrollFunction(); },300)};
-
-const fadeHeader = document.querySelectorAll(".header-top, .header-search, .header-button-a");
 const header = document.getElementById("header");
 const headerTop = document.getElementById("header-top");
 const headLogo = document.getElementById("header-logo-main");
 const headSearch = document.getElementById("header-search");
 const headNav = document.getElementById("header-nav");
 const dropdownMenu = document.getElementById("dropdownMenu");
-const exitDropdownMenu = document.querySelectorAll("*:not(dropdownMenu)");
 
-function scrollFunction() {
+const goTop = document.getElementById("goTop");
+
+const exitDropdownMenu = document.querySelectorAll("*:not(dropdownMenu)");
+const fadeHeader = document.querySelectorAll(".header-top, .header-search, .header-button-a, .goTop");
+
+window.onscroll = function() {scrollFunctionHeader()};
+
+function scrollFunctionHeader() {
+    // När användaren skrollar ner 100px så göms Loggan i headern.
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         headerTop.style.maxHeight = "0px";
 
-        // setTimeout(() => { headNav.appendChild(headSearch); }, 300);
         headLogo.style.maxHeight = headerTop.style.maxHeight;
     } else {
         headerTop.style.maxHeight = "12.5vh";
 
-        // headerTop.prepend(headSearch);
-        // headerTop.prepend(headLogo);
         headLogo.style.maxHeight = "11vh";
+    }
+    // När användaren skrollar ner 200px så syns knappen som tar en till toppen av sidan.
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        goTop.style.display = "unset";
+    } else {
+        goTop.style.display = "none";
     }
 }
 
@@ -36,9 +41,6 @@ headNav.addEventListener("click", function () {
     }
 });
 
-// exitDropdownMenu.addEventListener("click", function () {
-//     dropdownMenu.classList.remove("show");
-// });
 
 window.addEventListener('click', function(e){
     if (!document.getElementById('header-nav').contains(e.target) && dropdownMenu.classList.contains("show")){
@@ -49,4 +51,4 @@ window.addEventListener('click', function(e){
             fadeHeader[i].classList.toggle('menu-fader');
         }
     }
-})
+});

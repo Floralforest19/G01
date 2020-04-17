7d857723c14405518a45410de017c0f84590e1ee
rstina <53047495+rstina@users.noreply.github.com>
2020-04-17 10:40:10 +0200

    delete, add, 2 chars or more, less than 30 chars

--- admin/read-cat.php
+++ admin/read-cat.php
@@ -9,7 +9,7 @@
       href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap"
       rel="stylesheet"
   />
-  <title>Webbshop - Skapa kategori</title>
+  <title>Webshop - Skapa kategori</title>
 </head>
 <body>
 <script src="createvalidate.js"></script>
@@ -21,57 +21,60 @@
 
 <section class='background'>
 
-<h2>Kategorier</h2>
+  <h2>Kategorier</h2>
 
-<div class="box__search--form">
-  <form action="#" method="post" name="createCatForm"  onsubmit="return validateCatForm()">
-    <input name="catname" type="text" required placeholder="Kategori...">
-    <input type="submit" value="Lägg till kategori" class="contact-form-button">
-  </form>
-    <p id="feedbackCat" class="search__feedback"></p>
-</div>
+  <div class="box__cat--form">
+    <form action="#" method="post" name="createCatForm"  onsubmit="return validateCatForm()">
+      <input name="catname" type="text" class="input__cat" required placeholder="Kategori...">
+      <input type="submit" value="Lägg till kategori" class="cat-form-btn">
+    </form>
+      <p id="feedbackCat" class="search__feedback"></p>
 
-<?php require_once '../db.php';
 
-  // skapa kategori
-  if ( $_SERVER['REQUEST_METHOD'] === 'POST' ){
-    $catname = htmlspecialchars($_POST['catname']);
-    $sql = "INSERT INTO category (name) 
-            VALUES ( :name)";
+
+  </div>
+
+  <div class="box__cat--form">
+    <table class='table__cat'>
+  <?php require_once '../db.php';
+
+    // skapa kategori
+    if ( $_SERVER['REQUEST_METHOD'] === 'POST' ){
+      $catname = htmlspecialchars($_POST['catname']);
+      $sql = "INSERT INTO category (name) 
+              VALUES ( :name)";
+      $stmt = $db->prepare($sql);
+      $stmt->bindParam(':name' , $catname);
+      $stmt->execute();
+    } 
+
+    // visa kategorierna
+    $sql = "SELECT * FROM category 
+            ORDER BY name";
     $stmt = $db->prepare($sql);
-    $stmt->bindParam(':name' , $catname);
     $stmt->execute();
-  } 
-
-  // visa kategorierna
-  $sql = "SELECT * FROM category 
-          ORDER BY name";
-  $stmt = $db->prepare($sql);
-  $stmt->execute();
-
-  echo "<article class='box__search'>
-  <div class='box__text'>";
-  while($row = $stmt->fetch(PDO::FETCH_ASSOC)) :
-    $name = htmlspecialchars($row['name']);
-    $id = htmlspecialchars($row['category_id']);
-
-    echo "<h3>$name</h3>
-    <a href='edit-cat.php?id=$id'>Redigera</a>";
-
-    // man ska bara kunna radera en tom kategori
-    $sqlCheckIfProdsExist = "SELECT product_id FROM product WHERE category_id = $id";
-    $stmtCheckIfProdsExist = $db->prepare($sqlCheckIfProdsExist);
-    $stmtCheckIfProdsExist->execute();
-    $productrow = $stmtCheckIfProdsExist->fetch(PDO::FETCH_ASSOC);  
-    // ifall raden är 0 så är kategorin tom på kategorier
-    if( $productrow == 0 ){
-      echo " <br><a onclick=\"return confirm('Är du säker att du vill radera kategorin?')\" href='delete-cat.php?id=$id'>Radera</a>"; 
-    } else {
-      echo "<p>Går ej att radera</p>";
-    }
-  endwhile;
-  echo "</div></article></section>";
-
-?>
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC)) :
+      echo "<tr>";
+      $name = htmlspecialchars($row['name']);
+      $id = htmlspecialchars($row['category_id']);
+      echo "<h3>$name</h3>
+      <a href='edit-cat.php?id=$id'><button class='btn__edit'>Redigera</button></a>";
+      // man ska bara kunna radera en tom kategori
+      $sqlCheckIfProdsExist = "SELECT product_id FROM product WHERE category_id = $id";
+      $stmtCheckIfProdsExist = $db->prepare($sqlCheckIfProdsExist);
+      $stmtCheckIfProdsExist->execute();
+      $productrow = $stmtCheckIfProdsExist->fetch(PDO::FETCH_ASSOC);  
+      // ifall raden är 0 så är kategorin tom på kategorier
+      if( $productrow == 0 ){
+        echo "<p><a href='delete-cat.php?id=$id' onclick=\"return confirm('Är du säker att du vill radera kategorin?')\"><button  class='btn__delete'>Radera</button></a><p>"; 
+      } else {
+        echo "<p>Går ej att radera</p>";
+      }
+      echo "</div></tr>";
+    endwhile;
+  ?>
+    </table>
+  </div>
+</section>
 </body>
 </html>

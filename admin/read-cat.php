6b98f5654d7b494376a054961c5f08b18bf23525
rstina <53047495+rstina@users.noreply.github.com>
2020-04-19 12:36:35 +0200

    Categories (#29)
    * search and cat errors fixed, not language yet
    
    * validate for latin chars

--- admin/read-cat.php
+++ admin/read-cat.php
@@ -2,6 +2,7 @@
   * read info from db & show, add categories
 **************************************** */
 require_once 'header-admin.php';
+require_once '../db.php';
 ?>
 
 <section class='background'>
@@ -9,30 +10,38 @@ require_once 'header-admin.php';
   <h2>Kategorier</h2>
 
   <div class="box__cat--form box__cat--form--main">
-    <form action="#" method="post" name="createCatForm"  onsubmit="return validateCatForm()">
+    <form action="#" method="post" name="createCatForm" accept-charset="UTF-8" onsubmit="return validateCatForm()">
       <input name="catname" type="text" class="input__cat" required placeholder="Lägg till kategori...">
       <input type="submit" value="Lägg till kategori" class="cat-form-btn">
     </form>
-      <p id="feedbackCat" class="search__feedback"></p>
+    <p id="feedbackCat" class="search__feedback"></p>
+    <?php 
+      // skapa kategori
+      if ( $_SERVER['REQUEST_METHOD'] === 'POST' ){
+        $catname = htmlspecialchars(ucfirst(strtolower($_POST['catname'])));
+        // kolla om kategorin finns i databasen
+        $sql = "SELECT `name` FROM `category` WHERE `name` LIKE '$catname'";
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+        // $result sätts till true ifall kategorin existerar
+        $result = false; 
+        while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+          $result = true; 
+          echo "<p class='search__feedback'>$catname är redan en kategori</p>";
+        } // skapa ny kategori ifall nytt värde
+        if(!$result){
+          $sql = "INSERT INTO category (name) VALUES ( :name)";
+          $stmt = $db->prepare($sql);
+          $stmt->bindParam(':name' , $catname);
+          $stmt->execute();
+        }
+      }
+    ?>
   </div>
 
-  
-    
-  <?php require_once '../db.php';
-
-    // skapa kategori
-    if ( $_SERVER['REQUEST_METHOD'] === 'POST' ){
-      $catname = htmlspecialchars(ucfirst($_POST['catname']));
-      $sql = "INSERT INTO category (name) 
-              VALUES ( :name)";
-      $stmt = $db->prepare($sql);
-      $stmt->bindParam(':name' , $catname);
-      $stmt->execute();
-    } 
-
+<?php
     // visa kategorierna
-    $sql = "SELECT * FROM category 
-            ORDER BY name";
+    $sql = "SELECT * FROM category ORDER BY name";
     $stmt = $db->prepare($sql);
     $stmt->execute();
     while($row = $stmt->fetch(PDO::FETCH_ASSOC)) :
@@ -42,13 +51,15 @@ require_once 'header-admin.php';
       echo "<h3>$name</h3>
       <a href='edit-cat.php?id=$id'><button class='btn__edit'>Redigera</button></a>";
       // man ska bara kunna radera en tom kategori
-      $sqlCheckIfProdsExist = "SELECT product_id FROM product WHERE category_id = $id";
-      $stmtCheckIfProdsExist = $db->prepare($sqlCheckIfProdsExist);
-      $stmtCheckIfProdsExist->execute();
-      $productrow = $stmtCheckIfProdsExist->fetch(PDO::FETCH_ASSOC);  
+      $sqlCheckForProd = "SELECT product_id FROM product WHERE category_id = $id";
+      $stmtCheckForProd = $db->prepare($sqlCheckForProd);
+      $stmtCheckForProd->execute();
+      $productrow = $stmtCheckForProd->fetch(PDO::FETCH_ASSOC);  
       // ifall raden är 0 så är kategorin tom på kategorier
       if( $productrow == 0 ){
-        echo "<a href='delete-cat.php?id=$id' onclick=\"return confirm('Är du säker att du vill radera kategorin?')\"><button  class='btn__delete'>Radera</button></a>"; 
+        echo "<a href='delete-cat.php?id=$id' 
+        onclick=\"return confirm('Är du säker att du vill radera kategorin $name?')\">
+        <button class='btn__delete'>Radera</button></a>"; 
       } else {
         echo "<p>Går ej att radera, innehåller produkter</p>";
       }

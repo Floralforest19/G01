c6a908d9e46eafd1e79418a43550525e6fc203e8
Hannesdottir <>
2020-04-22 21:53:54 +0200

    shopping cart
    *Local storage functionality
    *Create shopping cart
    *Update shopping cart - Add products
    *Functionality to add products to cart in read.php

--- index.php
+++ index.php
@@ -5,7 +5,12 @@
 ?>
 
 
-  <!-- Slut på CSS-test -->
+<!-- Slut på CSS-test -->
 </main>
+
+<script type="text/javascript" src="js/cart-localstorage.js"></script>
+<script type="text/javascript" src="js/cart-add-product.js"></script>
+
 </body>
+
 </html>

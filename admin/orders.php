8337e6ff1184f38ae0db0749deee909daae06c4c
rstina <53047495+rstina@users.noreply.github.com>
2020-04-22 09:40:17 +0200

    Orders (#44)
    * search and cat errors fixed, not language yet
    
    * validate for latin chars
    
    * checks for spaces
    
    * sorting nav
    
    * fixat error
    
    * validate inputs
    
    * general validate input
    
    * general validate
    
    * header on search and main page
    
    * changed location of </main>
    
    * right validation for search
    
    * search keyword, removed bar, added column
    
    * preparing for orders
    
    * moved prod sort to menu, doesn't show if 0 qntiy
    
    * updated db
    
    * with keywords
    
    * get orders, sort asc ordrid, cust-id, time, amount
    
    * orders
    
    * order status
    
    * kr

--- admin/orders.php
+++ admin/orders.php
@@ -5,56 +5,85 @@
 
 
 <section class='background'>
-
   <h2>Beställningar</h2>
-  <div class='box__cat--form'>
-    <a href='orders.php?id=name'>Sortera på namn</a>
-    <a href='orders.php?id=id'>Sortera på id</a>
+  <div class="box__cat--form">
+  <div class='nav__admin'>
 
 <?php
 
-  if(isset($_GET['id'])){
+  if( isset($_GET['id']) ){
     $id = htmlentities($_GET['id']);
     // hämta från beställningar istället
-    if($id == 'name'){
-      $sql = "SELECT * FROM product
-              ORDER BY name";
-    } elseif($id == 'id'){
-      $sql = "SELECT * FROM product
-              ORDER BY category_id";
-    }
+    $sql = "SELECT * FROM orders ORDER BY $id ASC";
   } else {
-    $sql = "SELECT * FROM product
-            ORDER BY name";
+    $sql = "SELECT * FROM orders ORDER BY order_id";
   }
   $stmt = $db->prepare($sql);
   $stmt->execute();
 
   $tableOrders = "
   <table class='table'>
-    <tr>
-      <th>Order-Id</th>
-      <th>Kund</th>
-      <th>Beskrivning</th>
-      <th>Status</th>
-      <th></th>
-    </tr>";
+  <thead>
+    <th><a href='orders.php?id=order_id'>Order-id</a></th>
+    <th><a href='orders.php?id=customer_id'>Kund-id</a></th>
+    <th>Namn</th>
+    <th>E-mail</th>
+    <th>Telefon</th>
+    <th>Adress</th>
+    <th><a href='orders.php?id=time'>Tid/datum</a></th>
+    <th><a href='orders.php?id=amount'>Summa</a></th>
+    <th>Status</th>
+  </thead>";
     while($row = $stmt->fetch(PDO::FETCH_ASSOC)) :
-      $name = htmlspecialchars($row['name']);
-      $id = htmlspecialchars($row['category_id']);
-      $description = htmlspecialchars($row['description']);
-      $tableOrders.= "<tr>
-        <td><h4>$id</h4></td>
-        <td><a href='showorder.php?id=$id'><h3>$name</h3></a></td>
-        <td><p>$description</p></td>
-        <td><p>Status</p></td>
-        <td><a href='showorder.php?id=$id'><button class='btn__edit'>Arkivera</button></a></td>
-      </tr>";
+      // kolla igenom alla ordrar och spara order-id samt kundi-id
+      $order_id = htmlspecialchars($row['order_id']);
+      $customer_id = htmlspecialchars($row['customer_id']);
+      $status = htmlspecialchars($row['status']);
+      $amount = htmlspecialchars($row['amount']);
+      $time = htmlspecialchars($row['time']);
+      if($status == 'active'){
+        $statusText = 'Aktiv';
+      } else if($status == 'in progress') {
+        $statusText = 'Pågående';
+      } else if($status == 'done') {
+        $statusText = 'Avklarad';
+      } else {
+        $statusText = 'Oklar';
+      }
+
+      $sqlCustomer = "SELECT * FROM customers WHERE customer_id = $customer_id";
+      $stmtCustomer = $db->prepare($sqlCustomer);
+      $stmtCustomer->execute();
+      $rowCustomer = $stmtCustomer->fetch(PDO::FETCH_ASSOC);
+  
+      $fname = htmlspecialchars($rowCustomer['firstname']);
+      $sname = htmlspecialchars($rowCustomer['surname']);
+
+      $email = htmlspecialchars($rowCustomer['email']);
+      $phone = htmlspecialchars($rowCustomer['phone']);
+  
+      $street = htmlspecialchars($rowCustomer['streetadress']);
+      $zip = htmlspecialchars($rowCustomer['zip-code']);
+      $city = htmlspecialchars($rowCustomer['city']);
+
+      $tableOrders.= "
+      <tr>
+        <td><a href='showorder.php?id=$order_id'><h3>$order_id</h3></a></td>
+        <td><p>$customer_id</p></td>
+        <td><p>$fname $sname</p></td>
+        <td><p>$email</p></td>
+        <td><p>$phone</p></td>
+        <td><p>$street <br>$zip $city</p></td>
+        <td><p>$time</p></td>  
+        <td><p>$amount kr</p></td>      
+        <td><br>$statusText</td>
+      </tr>
+    ";
     endwhile;
     $tableOrders .= "</table></div>";
     echo $tableOrders;
   ?>
-  </section>
+  </div></div></section>
   
 </section>
 </body>

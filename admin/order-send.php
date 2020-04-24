0ab4311cb380617ef6d41666a4d4ce6ad78828b9
rstina <53047495+rstina@users.noreply.github.com>
2020-04-24 14:22:32 +0200

    New logic for send order (#57)
    * search and cat errors fixed, not language yet
    
    * validate for latin chars
    
    * checks for spaces
    
    * sorting nav
    
    * fixat error
    
    * validate inputs
    
    * general validate input
    
    * general validate
    
    * header on search and main page
    
    * changed location of </main>
    
    * right validation for search
    
    * search keyword, removed bar, added column
    
    * preparing for orders
    
    * moved prod sort to menu, doesn't show if 0 qntiy
    
    * updated db
    
    * with keywords
    
    * get orders, sort asc ordrid, cust-id, time, amount
    
    * orders
    
    * order status
    
    * kr
    
    * change status on order
    
    * menu both api and active/done orders
    
    * in progress, asc dec
    
    * order by status, time status
    
    * removed select from finished orders
    
    * filter start
    
    * linking search
    
    * fixed delete-bug, edit-prod-bug, NEW DB!!!
    
    * show cart with buttons
    
    * show products
    
    * kr
    
    * center cart, items above customer info
    
    * send-order

---
+++ admin/order-send.php
@@ -0,0 +1,54 @@
+<?php 
+  /**************************************** *
+    * read info from db & show, add categories
+  **************************************** */
+  require_once 'header-admin.php';
+  require_once '../db.php'; 
+
+  // get current name and set as placeholder in form
+  if(isset($_GET['id'])){
+    $id = htmlspecialchars($_GET['id']); 
+    $sql = "SELECT name FROM category WHERE category_id = $id";
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    $name = htmlspecialchars($row['name']);
+  }
+?>
+
+<section class='background'>
+  <h2>Redigera kategori</h2>
+  <div class="box__cat--form box__cat--form--main">
+    <form action="#" method="post" name="createCatForm" accept-charset="UTF-8" onsubmit="return validateForm('createCatForm','catname','feedbackCat')">
+      <input name="catname" type="text" class="input__cat" required placeholder="<?php echo $name ?>">
+      <?php // kolla om kategori existerat --> ge feedback
+        if(isset($_POST['catname'])){
+          $id = htmlspecialchars($_GET['id']); 
+          $catname = ucfirst(htmlspecialchars($_POST['catname'])); 
+          // kolla om kategorin finns i databasen
+          $sql = "SELECT `name` FROM `category` WHERE `name` LIKE '$catname'";
+          $stmt = $db->prepare($sql);
+          $stmt->execute();
+          // $result sätts till true ifall kategorin existerar
+          $result = false; 
+          while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            $result = true; 
+            echo "<p class='search__feedback'>$catname är redan en kategori, ange annat värde.</p>";
+          } // skapa ny kategori ifall nytt värde
+          if(!$result){
+            $sql = "UPDATE category SET name = '$catname' WHERE category_id = '$id' ";
+            $stmt = $db->prepare($sql);
+            $stmt->execute();
+            header('Location:read-cat.php');
+          }
+        }
+      ?>
+      <p id="feedbackCat" class="search__feedback"></p>
+      <input type="submit" value="Byt kategorinamn" class="cat-form-btn"> 
+    </form>
+    <a href="read-cat.php"><button class="btn__delete">Avbryt</button></a>
+        
+  </div>
+</section>
+</body>
+</html>

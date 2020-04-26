e0c56bef08bd42949407347ac52937f85f880f8f
Mikaela Fryklund <mikaela@fryklunda.se>
2020-04-26 22:32:16 +0200

    orders-single fixed

--- orders-single.php
+++ orders-single.php
@@ -14,7 +14,7 @@
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
     $customer_id = htmlspecialchars($_GET['customer_id']);
-    $customer = "SELECT * FROM customers WHERE customer_id=$customer_id"; 
+    $customer = "SELECT * FROM customers WHERE customer_id=$customer_id";
     $stmt = $db->prepare($customer);
     $stmt->execute();
     $customerRow = $stmt->fetch(PDO::FETCH_ASSOC);
@@ -27,7 +27,6 @@
     $firstname = htmlspecialchars($customerRow['firstname']);
     $surname = htmlspecialchars($customerRow['surname']);
     $fullname = $firstname." ".$surname;
-
     $email = htmlspecialchars($customerRow['email']);
     $phone = htmlspecialchars($customerRow['phone']);
 
@@ -35,38 +34,60 @@
     $zip = htmlspecialchars($customerRow['zip-code']);
     $city = htmlspecialchars($customerRow['city']);
     $address = $street."<br>".$zip." ".$city;
-
     $thisPost = "
+   
+<h2>Tack för ditt köp!<br> Ett kvitto kommer att skickas till den mail du angivit</h2>   
 <section class='background'>
-<h2>Orderbekräftelse</h2>
 <div class='box__cat--form'>
+<table id='dispItems' class='display__order-items'>
 <div class='nav__admin'>
-  <table class='table'>
-    <thead>
-      <th>Order-id</th>
-      <th>Kund-id</th>
-      <th>Namn</th>
-      <th>E-mail</th>
-      <th>Telefon</th>
-      <th>Adress</th>
-      <th>Tid/datum</th>
-      <th>Summa</th>
-      
-    </thead>
+  <table class='display__order-items'>
     <tr>
+      <th>Order-id</th>
       <td><h3>$order_id</h3></td>
+    </tr>
+    <tr>  
+      <th>Kund-id</th>
       <td><p>$customer_id</p></td>
+    </tr>
+    <tr>  
+      <th>Namn</th>
       <td><p>$firstname</p></td>
+    </tr> 
+    <tr> 
+      <th>E-mail</th>
       <td><p>$email</p></td>
+    </tr> 
+    <tr>  
+      <th>Telefon</th>
       <td><p>$phone</p></td>
+    </tr>
+    <tr>
+      <th>Adress</th>
       <td><p>$address</p></td>
-      <td><p>$time</p></td>  
-      <td><p>$amount Kr</p></td>      
-      
+   </tr>   
+   <tr>   
+      <th>Tid/datum</th>
+      <td><p>$time</p></td>
+   </tr>
+   <tr>   
+      <th>Summa</th>
+      <td><p>$amount Kr</p></td>
     </tr>
   </table>
+  
 </div></div></section>";
 
     echo $thisPost;
-
+    
+ ?>   
+ <script src="js/order-show-items.js"></script>
+ <script>
+    function clearLocal(){
+        localStorage.clear()
+    }
+    clearLocal()
+</script>
+ <?php
+require_once 'footer.php';
 ?>

6712b206d5f9e36378c7458f6605e9b214d709b3
rebeckamolin <56819811+rebeckamolin@users.noreply.github.com>
2020-04-27 17:41:29 +0200

    tagit bort json. ska göra om

--- json_mysql.php
+++
@@ -1,8 +0,0 @@
-<script src="sending-json.js"></script>
-<?php
-require_once "db.php"
-
-$json_file = products
-
-?>
-
--- sending-json.js
+++
@@ -1,9 +0,0 @@
-// function getProducts() {
-//   let cartFromStorage = localStorage.getItem("shoppingCart");
-//   let cartObj = JSON.parse(cartFromStorage);
-//   let order_prod = cartObj.order_prod;
-//   getProdsToCart(order_prod);
-//   console.log(order_prod);
-// }
-// getProducts();
-

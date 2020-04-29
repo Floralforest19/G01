6adc7bc32b40c08923dc78de9fc4d1674ec91e4f
rstina <53047495+rstina@users.noreply.github.com>
2020-04-29 11:45:59 +0200

    search linking

--- searchfilter.php
+++ searchfilter.php
@@ -32,20 +32,22 @@ if(isset($_POST['input']) ){
       $image = 'toalettpapper.jpg';
     }
     if($quantity > 0){
+      if($quantity < 10){
+        $priceText = "<a href='showproduct.php?id=$id'><p class='sale__old'>$price kr</p></a>
+        <a href='showproduct.php?id=$id'><p class='sale__new'>".number_format($price*0.9,2)." kr</p></a>";
+      } else {
+        $priceText = "<a href='showproduct.php?id=$id'><p class=''>$price kr</p></a>";
+      }
     // product exist and in db and is in storage, show result
     echo "<article class='box__search'>
             <div class='box__pic--search'>
-              <img src='./images/$image' alt='$name'/>
+              <a href='showproduct.php?id=$id'><img src='./images/$image' alt='$name'/></a>
             </div>
             <div class='box__text--search'>
-              <h3>$name</h3>
-              <p>$price kr</p>
-              <p>$description</p>
-              <a href='showproduct.php?id=$id'>Läs mer</a><br>";
-              // lägga till när vi introducerar varukorg
-              // <button>Lägg i varukorg</button>
-              //<a href='#' class='product__btn'>Köp</a>
-              echo "
+              <a href='showproduct.php?id=$id'><h3>$name</h3></a>
+              $priceText
+              <a href='showproduct.php?id=$id'>Läs mer</a>
+              <p>$quantity i lager</p><br>
             </div>
           </article>";
     } else {

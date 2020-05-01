ada3fd3490ac166d186281999fdc79cb2f0fcde5
rstina <53047495+rstina@users.noreply.github.com>
2020-05-01 10:37:53 +0200

    filter and contact page

---
+++ admin/read-contact.php
@@ -0,0 +1,33 @@
+<?php /**************************************** *
+  * read info from db & show, add categories
+**************************************** */
+require_once 'header-admin.php';
+require_once '../db.php';
+?>
+
+<section class='background'>
+
+  <h2>Kontakt</h2>
+<?php
+    // visa kategorierna
+    $sql = "SELECT * FROM contactform ORDER BY contactemail";
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC)) :
+      echo "<div class='box__cat--form'><table class='table__cat'><tr>";
+      $id = htmlspecialchars($row['id']);
+      $name = htmlspecialchars($row['contactname']);
+      $message = htmlspecialchars($row['contactmessage']);
+      $phone = htmlspecialchars($row['contactphone']);
+      $email = htmlspecialchars($row['contactemail']);
+      echo "<h3>$name</h3>";
+      echo "<p>$email</p>";
+      echo "<p>$phone</p>";
+      echo "<p>$message</p>";
+      echo "</div></tr></table></div>";
+    endwhile;
+  ?>
+    
+</section>
+</body>
+</html>
--- js/filter-orders.js
+++ js/filter-orders.js
@@ -3,6 +3,7 @@ let tableOutput = document.createElement('table')
 tableOutput.classList.add('table')
 tableDiv.appendChild(tableOutput)
 
+// filtrera ordrar
 function filterPhrase() {
   let filterInput = document.getElementById('filterInput')
   filterInput.addEventListener('input', function (e) {
@@ -15,23 +16,26 @@ function filterPhrase() {
     })
   })
 }
+// filtrera
 filterPhrase()
 
+// hämta order info
 fetch('orders-information.php')
   .then(resp => resp.json())
   .then(order => showOrders(order))
+
+// visa ordar i DOM
 function showOrders(order) {
-  let table = ''
-  table += `
+  let table = `
       <thead>
         <th>Order-id</th>
         <th>Namn</th>
         <th>E-mail</th>
         <th>Leveransadress</th>
         <th>Tid/Datum</th>
+        <th>Sum</th>
         <th>Status</th>
       </thead>`
-
   for (let i = 0; i < order.length; i++) {
       selectStatus =
         `<form method='post' action='orders-update.php?order_id=${order[i].order_id}'>
@@ -48,17 +52,18 @@ function showOrders(order) {
           <option value='done'>Slutförd</option>`
       }
       selectStatus += `</select><input type='submit' value='Sätt status'></form>`
-
       table +=
         `<tr>
-          <td> ${order[i].order_id} </td> 
-          <td> ${order[i].name} </td> 
-          <td> ${order[i].email} </td> 
-          <td> ${order[i].shippingStreet} ${order[i].shippingZip} <br>
-          ${order[i].shippingCity} </td> 
-          <td> ${order[i].time} </td> 
-          <td> ${selectStatus} </td> 
+          <td>${order[i].order_id}</td> 
+          <td>${order[i].name}</td> 
+          <td>${order[i].email}</td> 
+          <td>${order[i].shippingStreet} ${order[i].shippingZip}<br>
+              ${order[i].shippingCity}</td> 
+          <td id="orderByTime">${order[i].time}</td> 
+          <td id="orderBySum">SUMMA</td> 
+          <td id="orderByStatus">${selectStatus}</td> 
         </tr>`
+        // lägga till summa
   }
   tableOutput.innerHTML = table
 }

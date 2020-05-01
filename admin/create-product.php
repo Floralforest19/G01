c71a241931ddbde74aff1b7d0c4074e92e723609
carlesonc <christopher.carleson@gmail.com>
2020-05-01 13:46:18 +0200

    G01-98 - Knapp för att gå tillbaka från skapa-produkt sidan. KLAR och redo för merge

--- admin/create-product.php
+++ admin/create-product.php
@@ -33,10 +33,10 @@ require_once '../db.php';
    <div> <textarea name="description" type="text" cols="30" rows="5" class="input__cat" required placeholder="Beskrivning"> </textarea></div> 
    <div>  <input name="quantity" type="number" class="input__cat" required placeholder="Antal"></div> 
    <div>  <input name="price" type="number" class="input__cat" required placeholder="Pris"></div> 
+   
     <div style="display: flex;justify-content: center;justify-content: space-evenly;">
       <a class="btn__delete " href="index.php" style="margin: 0;text-decoration: none;text-align: center;font-weight: 600;padding-top: 3px;">Avbryt</a>
       <input class="product__btn" type="submit" value="Lägg till produkt">
     </div>
 </form>
-
 </div>

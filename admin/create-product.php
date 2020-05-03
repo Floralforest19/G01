5c01525e21c203f5d6c266106cf3da742542b8b2
LinaLin123 <jlina.lin@gmail.com>
2020-05-03 02:04:10 +0200

    validate create product

--- admin/create-product.php
+++ admin/create-product.php
@@ -2,13 +2,13 @@
 <h2>Skapa produkt</h2>
 
 <div class="wrap">
-<form action="upload-pro.php" method="post" enctype="multipart/form-data" name="createProdForm" class="wrap">
+<form action="upload-pro.php" method="post" enctype="multipart/form-data" name="createProdForm" onsubmit="return validateAll()" class="wrap">
     <?php
       if (isset($_GET['uppladdning']) == true) {
         echo "<h4 style='color: red;'>En produkt får max ha 5 bilder.</h4>";
       }
     ?>
-    <div>  <input type="file" name="image_file_name[]" id="image_file_name" type="text" class="input__cat" placeholder="Bild" multiple></div> 
+    <div> <input type="file" name="image_file_name[]" id="image_file_name" type="text" class="input__cat" placeholder="Bild" multiple></div> 
    
     <div> <?php
 // visa kategorierna
@@ -29,10 +29,27 @@ require_once '../db.php';
 ?>
 </div> 
       <!-- Dessa inputs är required -->
-   <div> <input name="name" type="text" class="input__cat" required placeholder="Produktnamn"></div> 
-   <div> <textarea name="description" type="text" cols="30" rows="5" class="input__cat" required placeholder="Beskrivning"> </textarea></div> 
-   <div>  <input name="quantity" type="number" class="input__cat" required placeholder="Antal"></div> 
-   <div>  <input name="price" type="number" class="input__cat" required placeholder="Pris"></div> 
+   <div> <label for="name">Namn:</label> 
+   <input name="name" type="text" class="input__cat" required placeholder="Produktnamn">
+   <div id="nameFeedback" style="color:#eb4b88"></div>
+  </div> 
+   
+  <div> <label for="description"> Beskrivning:</label>
+     <textarea name="description" type="text" cols="30" rows="5" class="input__cat" required placeholder="Beskrivning"> 
+  </textarea>
+  <div id="descriptionFeedback" style="color:#eb4b88"></div>
+
+</div> 
+   
+  <div> <label for="quantity"> Antal:</label>
+      <input name="quantity" type="number" class="input__cat" required placeholder="Antal">
+      <div id="quantityFeedback" style="color:#eb4b88"></div>
+  </div> 
+   
+    <div> <label for="price"> Pris:</label>
+      <input name="price" type="number" class="input__cat" required placeholder="Pris">
+      <div id="priceFeedback" style="color:#eb4b88"></div>
+    </div> 
    
     <div style="display: flex;justify-content: center;justify-content: space-evenly;">
       <a class="btn__delete del" href="index.php" style="text-decoration: none;text-align: center;font-weight: 600;padding-top: 3px;">Avbryt</a>
@@ -40,3 +57,37 @@ require_once '../db.php';
     </div>
 </form>
 </div>
+
+<!-- Validering -->
+<script src="../js/validate-updateproduct.js"></script>
+
+<script>
+function validateAll() {
+    let isAllValidated = true;
+    let nameValidated = validateTextInput('createProdForm', 'name', 'nameFeedback');
+    if (nameValidated == false) {
+        isAllValidated = false;
+    }
+
+    let descriptionValidated = validateTextDescriptionInput('createProdForm', 'description', 'descriptionFeedback');
+    if (descriptionValidated == false) {
+        isAllValidated = false;
+    }
+
+    let quantityValidated = validateNumberInput('createProdForm', 'quantity', 'quantityFeedback');
+    if (quantityValidated == false) {
+        isAllValidated = false;
+    }
+
+    let priceValidated = validateNumberInput('createProdForm', 'price', 'priceFeedback');
+    if (priceValidated == false) {
+        isAllValidated = false;
+    }
+
+
+    if (isAllValidated == false) {
+        return false;
+    }
+    return true;
+}
+</script>

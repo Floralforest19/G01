2eb0dd1e33f3b6acd83b8d76af81f79847102b9b
rstina <53047495+rstina@users.noreply.github.com>
2020-05-04 17:52:07 +0200

    allow , in addresses

--- validateinput.js
+++ validateinput.js
@@ -99,3 +99,39 @@ function validateTextarea(formName, inputName, feedbackId) {
   }
   return true;
 }
+
+
+// checks number of characters entered
+function validateAddress(formName, inputName, feedbackId) {
+  let input = document.forms[formName][inputName].value;
+  let onlyLetters = /^[a-zA-Z\- ÅåÄäÖöØøÆæÉéÈèÜüÊêÛûÎî0123456789,/]*$/.test(
+    input
+  );
+  let findSpaces = /^(\s\s)/.test(input);
+
+  // check if right input lenght
+  if (input.length < 2 || input.length > 30) {
+    if (input.length > 30) {
+      document.getElementById(feedbackId).innerHTML =
+        "Måste vara kortare än 30 tecken";
+    } else {
+      document.getElementById(feedbackId).innerHTML =
+        "Skriv in minst två tecken";
+    }
+    return false;
+  }
+
+  // check for latin letters
+  else if (onlyLetters == false) {
+    document.getElementById(feedbackId).innerHTML =
+      "Endast tecken från latinska alfabeten tillåtet";
+    return false;
+  }
+
+  // check for mutliple spaces in begining of string
+  else if (findSpaces == true) {
+    document.getElementById(feedbackId).innerHTML =
+      "För många mellanslag i rad";
+    return false;
+  }
+}

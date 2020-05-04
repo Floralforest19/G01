664f1df496f7562b671a3b94be3f0140811f6bc0
rstina <53047495+rstina@users.noreply.github.com>
2020-05-04 17:44:00 +0200

    validate inpu including name, zero allowed

--- js/validate-updateproduct.js
+++ js/validate-updateproduct.js
@@ -1,7 +1,7 @@
 function validateTextDescriptionInput(formName, inputName, feedbackId) {
   document.getElementById(feedbackId).innerHTML = "";
   let input = document.forms[formName][inputName].value;
-  let onlyLetters = /^[a-zA-Z\- ÅåÄäÖöØøÆæÉéÈèÜüÊêÛûÎî0123456789.?]*$/.test(
+  let onlyLetters = /^[a-zA-Z\- ÅåÄäÖöØøÆæÉéÈèÜüÊêÛûÎî0123456789.!,?%]*$/.test(
     input
   );
 
@@ -67,6 +67,41 @@ function validatePriceInput(formName, inputName, feedbackId) {
       document.getElementById(feedbackId).innerHTML =
         "Måste vara kortare än 6 siffrigt belopp";
     }
+    return false;
+  }
+
+  return true;
+}
+
+function validateTextInput(formName, inputName, feedbackId) {
+  document.getElementById(feedbackId).innerHTML = "";
+  let input = document.forms[formName][inputName].value;
+  let onlyLetters = /^[a-zA-Z\- ÅåÄäÖöØøÆæÉéÈèÜüÊêÛûÎî0123456789.!,?%]*$/.test(
+    input
+  );
+
+  let findSpaces = /^(\s\s)/.test(input);
+
+  // check if right input lenght
+  if (input.length < 2 || input.length > 300) {
+    if (input.length > 300) {
+      document.getElementById(feedbackId).innerHTML =
+        "Måste vara kortare än 300 tecken";
+    } else {
+      document.getElementById(feedbackId).innerHTML =
+        "Skriv in minst två tecken";
+    }
+    return false;
+  } else if (onlyLetters == false) {
+    document.getElementById(feedbackId).innerHTML =
+      "Endast tecken från latinska alfabeten tillåtet";
+    return false;
+  }
+
+  // check for mutliple spaces in begining of string
+  else if (findSpaces == true) {
+    document.getElementById(feedbackId).innerHTML =
+      "För många mellanslag i rad";
     return false;
   }
 

940bd90ef495f6981331feae275d6239f09fb92f
rstina <53047495+rstina@users.noreply.github.com>
2020-05-07 14:19:24 +0200

    font-awesome

--- admin/header-admin.php
+++ admin/header-admin.php
@@ -5,11 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <link rel="stylesheet" type="text/css" href="styles/style.css" />
-  <link
-      href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap"
-      rel="stylesheet"
-  />
-  <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
   <title>Webshop v3.0 - Admin</title>
 </head>
